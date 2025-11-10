4ed252d2607fed038efe591fb2765b7cb8975e32
gabriel <gabrielsuarezcifuentes1221@gmail.com>
2025-11-09 23:24:19 -0500

    Corregido Router ante el sistema de Ruteo de github e implementacion de pagina 404

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,18 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Biblioteca from "../pages/Biblioteca";
 import Resenas from "../pages/Resenas";
 import Estadisticas from "../pages/Estadisticas";
 
 export default function Router() {
     return (
-        <Routes>
-            <Route path="/" element={<Navigate to="/biblioteca" replace />} />
-            <Route path="/biblioteca" element={<Biblioteca />} />
-            <Route path="/resenas" element={<Resenas />} />
-            <Route path="/estadisticas" element={<Estadisticas />} />
-        </Routes>
+        <BrowserRouter basename="/Frontend-GameTracker/">
+            <Routes>
+                <Route path="/biblioteca" element={<Biblioteca />} />
+                <Route path="/resenas" element={<Resenas />} />
+                <Route path="/estadisticas" element={<Estadisticas />} />
+                <Route path="/" element={<Navigate to="/biblioteca" replace />} />
+            </Routes>
+        </BrowserRouter>
+
     );
 }
