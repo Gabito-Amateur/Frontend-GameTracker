abc0cf27e73d8c39026fc5e34c75f82fc925f5c4
gabriel <gabrielsuarezcifuentes1221@gmail.com>
2025-11-11 22:53:16 -0500

    Revertido proyecto al commit Conectado Frontend con API (Render) + (Localmente)

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,18 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Biblioteca from "../pages/Biblioteca";
 import Resenas from "../pages/Resenas";
 import Estadisticas from "../pages/Estadisticas";
 
 export default function Router() {
     return (
-        <BrowserRouter basename="/Frontend-GameTracker/">
-            <Routes>
-                <Route path="/biblioteca" element={<Biblioteca />} />
-                <Route path="/resenas" element={<Resenas />} />
-                <Route path="/estadisticas" element={<Estadisticas />} />
-                <Route path="/" element={<Navigate to="/biblioteca" replace />} />
-            </Routes>
-        </BrowserRouter>
-
+        <Routes>
+            <Route path="/" element={<Navigate to="/biblioteca" replace />} />
+            <Route path="/biblioteca" element={<Biblioteca />} />
+            <Route path="/resenas" element={<Resenas />} />
+            <Route path="/estadisticas" element={<Estadisticas />} />
+        </Routes>
     );
 }
